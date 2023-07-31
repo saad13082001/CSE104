b1e922f5896641a025cf438002b3806656961f84
saad13082001 <140999614+saad13082001@users.noreply.github.com>
2023-07-31 10:40:34 +0600

    Add files via upload

---
+++ loop.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(;;)
+    {
+        if(i>=22)
+        {
+            break;
+        }
+        printf("%d\n",i+=2);
+    }
+
+
+
+
+}
---
+++ whileloop.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int i=1;
+
+
+    while(i<101)
+    {
+        printf("%d\n",i);
+        i+=2;
+        if(i>23)
+        {
+            break;
+        }
+
+    }
+
+
+
+}
